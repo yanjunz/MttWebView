7ca80f5a927b5234d9c739fd15a0c9452d4e5ecc
Yanjun Zhuang <zhuangyanjun@gmail.com>
2015-11-03 10:43:01 +0800

    Initial Commit

---
+++ WKBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKBrowser
+//
+//  Created by Yanjun Zhuang on 3/11/15.
+//  Copyright © 2015 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKBrowser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WKBrowser
+//
+//  Created by Yanjun Zhuang on 3/11/15.
+//  Copyright © 2015 Tencent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
